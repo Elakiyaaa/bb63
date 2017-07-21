584160daeb6e62708e1b07580441b47f979fd629
Elakiyaaa <elakthamizh@gmail.com>
2017-07-21 16:59:48 +0530

    Add files via upload

---
+++ hh55.java
@@ -0,0 +1,20 @@
+package GUVI;
+
+import java.util.Scanner;
+
+public class hh55 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.nextLine();
+int count=0;
+for(int i=0;i<a.length();i++){
+	
+	count++;
+}
+System.out.println(count);
+}
+	}
+
+
